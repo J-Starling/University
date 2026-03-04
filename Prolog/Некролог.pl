27b6842b0a7884f7c55118a933af62fb3b9a5b02
Jack <aksenovdmitriy1006@gmail.com>
2026-03-05 02:41:57 +0300

    Update Некролог.pl

--- Prolog/Некролог.pl
+++ Prolog/Некролог.pl
@@ -1,17 +1,23 @@
+%сумма натуральных чисел
 add(A,0,A):- !.
 add(A,s(B),s(C)):- add(A,B,C).
 
+%произведение натуральных чисел
 mult(0,B,0):- !.
 mult(s(A),B,C):- add(X,B,C), mult(A,B,X).
 
+%сравнение натуральных чисел (A больше B?)
 g_t(s(A),0):- !.
 g_t(s(A),s(B)):-g_t(A,B).
 
+%НОД
 gcd(A, 0, A) :- !.
 gcd(A, B, C) :- not(0 is B), R is A mod B, gcd(B, R, C).
 
+%целочисленное деление натуральных чисел с остатком
 q_r(A, B, C, D) :- C is A div B, D is A mod B.
 
+%N-е простое число
 prime(N, A) :- findnsols(N,B,p(B),L), !, last(L,A).
 pint(1).
 pint(X):- pint(Y), X is Y+1.
@@ -19,6 +25,7 @@ p(A):- pint(A), A>1, A1 is A - 1, notdiv(A,A1).
 notdiv(_,1):- !.
 notdiv(A,B):- B>1, X is A mod B, X\=0, B1 is B - 1, notdiv(A,B1).
 
+%список первых N чисел последовательности Фибоначчи
 fib(1,[1]):- !.
 fib(2,[1,1]):- !.
 fib(N,A):- N>2, N1 is N-1, N2 is N-2, fib(N1, B1), last(B1, A1), fib(N2, B2), last(B2, A2), 
